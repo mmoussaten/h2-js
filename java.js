847ab778aa9ad5a663c303bfa9fffd32969a86c7
marwan <marwan0320@live.nl>
2020-05-10 14:39:12 +0200

    Initial commit

---
+++ java.js
@@ -0,0 +1,6 @@
+document.getElementById("paginatitel").innerText ="Mijn geweldige pagina";
+document.getElementById("subkop").innerText ="Mijn geweldige subkop";
+document.getElementById("aap1").src= "img/aap.jpg";
+document.getElementById("vos").src= "img/vos.jpg";
+document.getElementById("blauw").style.marginBottom = "50px";
+document.getElementById("groen").style.marginTop = "100px";
